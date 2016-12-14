3789201314f4d4adf4cb9a853c681894739c392a
Scramasax <scramasax443@gmail.com>
2016-12-14 15:45:49 +0100

    second slideshow

--- astro/script/singlestudio.js
+++ astro/script/singlestudio.js
@@ -15,16 +15,19 @@ function currentDiv(n) {
 
 function showDivs(n) {
     var i;
-    var x = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
+    var x = document.getElementsByClassName("imgslide");
+    var dots = document.getElementsByClassName("imageproperties");
     if (n > x.length) {slideIndex = 1}
     if (n < 1) {slideIndex = x.length}
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+        dots[i].className = dots[i].className.replace("opacity", " ");
     }
     x[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " w3-opacity-off";
+    for (i = 0; i < dots.length; i++) {
+        if(i!=slideIndex-1)
+          dots[i].className += "opacity";
+    }
 }
