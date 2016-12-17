90c8fe81a961d06f25c0c23eb98ab17fe32b00c4
BottCode <mattiabottaro9@gmail.com>
2016-12-17 20:25:02 +0100

    Aggiunto backend per votazione di fotosingola e studiosingolo

--- astro/backend/fotosingola.php
+++ astro/backend/fotosingola.php
@@ -1,7 +1,7 @@
 <?php
      include "connessione.php";
     $idft=1; // parametro arrivante
-    
+
     $data_foto = $connessione->query("SELECT foto.*,astrofilo.username,astrofilo.imgprofilo
             FROM foto JOIN astrofilo ON foto.idastrofilo=astrofilo.mail
             WHERE foto.id='$ft'AND foto.idastrofilo IS NOT NULL");
@@ -12,11 +12,11 @@
            FROM giudicafoto
            WHERE idfoto='$idft'");
     $rank_foto=$rank_foto->fetch_array(MYSQLI_ASSOC);
-    
+
     echo " ".$data_foto['username']."| ".$rank_foto['rank']."] ";
-    
+
     // COMMENTI STUDIO
-    if(!$result=$connessione->query("SELECT astrofilo.username,astrofilo.imgprofilo,commentafoto.commento,commentafoto.datainserimento
+  if(!$result=$connessione->query("SELECT astrofilo.username,astrofilo.imgprofilo,commentafoto.commento,commentafoto.datainserimento
                FROM commentafoto JOIN astrofilo ON commentafoto.astrofilo=astrofilo.mail
                WHERE commentafoto.studio='$idft'
                ORDER BY commentafoto.datainserimento DESC")){
@@ -28,5 +28,19 @@
 			}
 			$result->free();
 		}
+    // voto=+1;-1;
+    // si può votare una sola volta per ogni foto
+
+  if(!$result=$connessione->query("SELECT votante FROM giudicafoto WHERE idfoto='$idft' AND votante='$mail_votante'")){
+			echo "Errore della query: ".$connessione->error.".";
+	}else{
+		if($result->num_rows<=0){ // vuol dire che può votare
+      $giudica_foto="INSERT INTO giudicafoto(votante,idfoto,voto) VALUES('$mail_votante','$idft','$voto')";
+      if(!$connessione->query($giudica_foto)){
+        //errore nell'insert
+      }
+      $result->free();
+		}
+
 	}
 ?>
--- astro/backend/studiosingolo.php
+++ astro/backend/studiosingolo.php
@@ -1,21 +1,21 @@
 <?php
     include "connessione.php";
     $idst=1; // parametro arrivante
-    
+
     $data_studio = $connessione->query("SELECT studia.*,astrofilo.username,astrofilo.imgprofilo
                    FROM studia JOIN astrofilo ON studia.astrofilo=astrofilo.mail
                    WHERE studia.idstudio='$idst'");
     //var_dump($datastudio);
     $data_studio=$data_studio->fetch_array(MYSQLI_ASSOC);
-    
+
     $foto_studio=$connessione->query("SELECT * FROM foto WHERE idstudio='$idst' LIMIT 3");
     $foto_studio=$foto_studio->fetch_array(MYSQLI_ASSOC);
 
     $rank_studio= $connessione->query("SELECT SUM(voto) AS rank FROM giudicastudio WHERE studio='$idst'");
     $rank_studio=$rank_studio->fetch_array(MYSQLI_ASSOC);
-    
+
     echo " [".$data_studio['titolo']."| ".$data_studio['username']."| ".$data_studio['appunti']."| ".$rank_studio['rank']."] ";
-    
+
     // COMMENTI STUDIO
     if(!$result=$connessione->query("SELECT astrofilo.username,astrofilo.imgprofilo,commentastudio.commento,commentastudio.datainserimento
                                      FROM commentastudio JOIN astrofilo ON commentastudio.astrofilo=astrofilo.mail
@@ -30,4 +30,21 @@
 			$result->free();
 		}
 	}
+
+  // voto=+1;-1;
+  // si può votare una sola volta per ogni studio
+
+if(!$result=$connessione->query("SELECT votante FROM giudicastudio WHERE idfoto='$idst' AND votante='$mail_votante'")){
+    echo "Errore della query: ".$connessione->error.".";
+}else{
+  if($result->num_rows<=0){ // vuol dire che può votare
+    $giudica_studio="INSERT INTO giudicastudio(votante,studio,voto) VALUES('$mail_votante','$idst','$voto')";
+    if(!$connessione->query($giudica_studio)){
+      //errore nell'insert
+    }
+    $result->free();
+  }
+
+}
+
 ?>
