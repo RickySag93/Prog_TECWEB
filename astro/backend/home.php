3992729af1fab025249952424f58e52bfb6cddc2
BottCode <mattiabottaro9@gmail.com>
2016-12-19 18:41:37 +0100

    Primi tentativi di comunicazione tra parte strutturale e comportamentale

--- astro/backend/home.php
+++ astro/backend/home.php
@@ -1,7 +1,7 @@
 <?php
 
     include "connessione.php";
-    // SLIDESHOW
+    /*// SLIDESHOW
     if(!$result=$connessione->query("SELECT *
         FROM foto JOIN fotobyrank ON foto.idfoto=fotobyrank.idfoto
         LIMIT 5")){
@@ -11,29 +11,34 @@
 			// while stampa immagini
 			$result->free();
 		}
-	}
+	}*/
 	$datetime_now=date("Y-m-d H:i:s");
-	// AVVENIMENTI FUTURI: quelli non ancora finiti. Dal piu` prossimo al piu` distante
-    if(!$result=$connessione->query("SELECT * FROM avvenimenti WHERE fine>'$datetime_now'ORDER BY inizio LIMIT 5")){
+  // AVVENIMENTI PASSATI: quelli finiti. Dal piu` recente al meno recente.
+  echo file_get_contents("../provaindex.html");
+  if(!$result=$connessione->query("SELECT * FROM avvenimenti WHERE fine<'$datetime_now'ORDER BY fine DESC LIMIT 5")){
+      echo "Errore della query: ".$connessione->error.".";
+  }else{
+    if($result->num_rows>0){
+      while($row=$result->fetch_array(MYSQLI_ASSOC)){
+        echo $row['tipo']." ".$row['inizio']."\n";
+      }
+      $result->free();
+    }
+  }
+
+  echo file_get_contents("../provaindex2.html");
+
+  // AVVENIMENTI FUTURI: quelli non ancora finiti. Dal piu` prossimo al piu` distante
+  if(!$result=$connessione->query("SELECT * FROM avvenimenti WHERE fine>'$datetime_now'ORDER BY inizio LIMIT 5")){
 			echo "Errore della query: ".$connessione->error.".";
 	}else{
 		if($result->num_rows>0){
 			while($row=$result->fetch_array(MYSQLI_ASSOC)){
-				echo $row['tipo']." ".$row['inizio']." // ";
+				echo $row['tipo']." ".$row['inizio']."\n";
 			}
 			$result->free();
 		}
 	}
-	// AVVENIMENTI PASSATI: quelli finiti. Dal piu` recente al meno recente.
-	if(!$result=$connessione->query("SELECT * FROM avvenimenti WHERE fine<'$datetime_now'ORDER BY fine DESC LIMIT 5")){
-			echo "Errore della query: ".$connessione->error.".";
-	}else{
-		if($result->num_rows>0){
-			while($row=$result->fetch_array(MYSQLI_ASSOC)){
-				echo $row['tipo']." ".$row['inizio']." // ";
-			}
-			$result->free();
-		}
-	}
+  echo file_get_contents("../provaindex3.html");
 
 ?>
