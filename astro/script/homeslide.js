687a7370d61f574cda71f1f72ec266e248a0cdab
Scramasax443 <scramasax443@gmail.com>
2016-12-20 11:14:33 +0100

    Add files via upload

--- astro/script/homeslide.js
+++ astro/script/homeslide.js
@@ -8,10 +8,13 @@ function plusDivs(n) {
 function showDivs(n) {
   var i;
   var x = document.querySelectorAll(".imgslide");
+  var tot=x.length;
   if (n > x.length) {slideIndex = 1;}
   if (n < 1) {slideIndex = x.length;}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";
   }
   x[slideIndex-1].style.display = "block";
+  var g=document.getElementsByClassName("number");
+  g.innerHTML = "bla";
 }
